757c131b6cb19e8929fddddb4f31b2b44d13eefb
skillzo <skillzfire@yahoo.com>
2023-04-08 22:29:53 +0100

    upload page

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function login() {
+  return (
+    <div>
+      <div className="bg-[#F4F6FE]  ">
+        <p></p>
+      </div>
+    </div>
+  );
+}
--- src/pages/marketplace/upload.jsx
+++ src/pages/marketplace/upload.jsx
@@ -1,43 +1,44 @@
 import ButtonPurple from "@/components/buttons/Buttons";
 import Label, { Select } from "@/components/tinyComp/Inputs";
-import Image from "next/image";
 import React, { useRef } from "react";
 
-export default function upload({ styleprops }) {
+export default function upload() {
   const inputRef = useRef(null);
   return (
-    <form className="space-y-[2em] lg:w-[70%] mx-auto">
-      <Label title="Title" />
+    <>
+      <form className="space-y-[2em] lg:w-[70%] mx-auto">
+        <Label title="Title" />
 
-      <div className="space-y-[2em] md:space-y-0 md:flex justify-between items-center">
-        <Select />
-        <Select />
-      </div>
-      <div className="space-y-[2em] md:space-y-0 md:flex justify-between items-center">
-        <Select />
-        <Select />
-      </div>
-
-      <div
-        onClick={() => inputRef.current.click()}
-        className="bg-[#270c4b] md:w-[90%] mx-auto pt-[3em] pb-[5em] flex justify-center border border-dashed"
-      >
-        <div className="flex flex-col items-center space-y-[2em]">
-          <div className="text-center">
-            <h2 className="text-2xl">Upload File</h2>
-            <p>(Video should not be more than 300mb)</p>
-          </div>
-
-          <input type="file" hidden ref={inputRef} />
-          <img src="/upload.svg" className="w-[40px] h-[40px]" />
+        <div className="space-y-[2em] md:space-y-0 md:flex justify-between items-center">
+          <Select />
+          <Select />
         </div>
-      </div>
+        <div className="space-y-[2em] md:space-y-0 md:flex justify-between items-center">
+          <Select />
+          <Select />
+        </div>
 
-      <div className="flex justify-center">
-        <ButtonPurple styleprops="w-[45%] md:w-[30%] lg:w-[20%] mx-auto">
-          Upload Property
-        </ButtonPurple>
-      </div>
-    </form>
+        <div
+          onClick={() => inputRef.current.click()}
+          className="bg-[#270c4b] md:w-[90%] mx-auto pt-[3em] pb-[5em] flex justify-center border border-dashed"
+        >
+          <div className="flex flex-col items-center space-y-[2em]">
+            <div className="text-center">
+              <h2 className="text-2xl">Upload File</h2>
+              <p>(Video should not be more than 300mb)</p>
+            </div>
+
+            <input type="file" hidden ref={inputRef} />
+            <img src="/upload.svg" className="w-[40px] h-[40px]" />
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <ButtonPurple styleprops="w-[45%] md:w-[30%] lg:w-[20%] mx-auto">
+            Upload Property
+          </ButtonPurple>
+        </div>
+      </form>
+    </>
   );
 }
